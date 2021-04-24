b2b7809066994dcd421e9ad47ff24bd6548e629d
david ez borges <david.ezborges@gmail.com>
2021-04-24 15:57:11 -0300

    added shel script to tweak dev/prod/test builds & enviroment variables declaration

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker build --build-arg SECRET_KEY='5vUCOVMkF2JNhH2CmzY4BnhDiiUtv+reNq2wwxbw31E=' --build-arg DB_NAME=craftek --build-arg DB_ENGINE=some-db-engine --build-arg DB_USER=craftech --build-arg DB_PASS=craftech --build-arg DB_HOST=http://localhost --build-arg DB_PORT=5432 .
+docker build \
+-t craftech:0.0.01
+--build-arg SECRET_KEY='5vUCOVMkF2JNhH2CmzY4BnhDiiUtv+reNq2wwxbw31E=' \
+--build-arg DB_NAME=craftek \
+--build-arg DB_ENGINE=some-db-engine \
+--build-arg DB_USER=craftech \
+--build-arg DB_PASS=craftech \
+--build-arg DB_HOST=http://localhost \
+--build-arg DB_PORT=5432 \
+--build-arg NODE_ENV=production \
+.
